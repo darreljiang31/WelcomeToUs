6dfe533331305f564360887d1be0908a9d6b082e
Sunho Han <sunho.han@monsanto.com>
2018-10-13 23:20:52 -0500

    added

---
+++ screens/WeNeedToGetToKnowYou.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Item, Input, Title, Text } from 'native-base';
+import DoubleButton from './DoubleButton'
+export default class WeNeedToGetToKnowYou extends React.Component {
+ static navigationOptions = {
+   header: null,
+ }
+
+ render() {
+   return (
+     <Container>
+     <Header/>
+     <Content>
+       <Text
+       style={{ fontFamily: 'HelveticaNeue-Light', fontSize: 32, width: 250, alignSelf:'center', textAlign: 'center', marginTop: 80}}
+       >
+We need to get to know you.
+       </Text>
+       <Text style={{marginTop:100,  alignSelf:'center', textAlign: 'center', fontSize: 15, width: 240, color: '#9b1ce0'}}>
+         Do you have time to give me a little information?
+       </Text>
+     </Content>
+     <DoubleButton text1='YES' text2='NOT RIGHT NOW'/>
+   </Container>
+   )
+ }
+}
