16b7c6de72f479340eef44a48415d022e20bbad4
JIANG, DARREL [AG/1000] <djiang31@yahoo.com>
2018-10-13 20:43:54 -0500

    Added carousel

--- screens/MentorScreen.js
+++ screens/MentorScreen.js
@@ -1,18 +1,96 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Button, Icon, Text } from 'native-base';
-import { StyleSheet } from 'react-native'
+import Carousel from 'react-native-snap-carousel';
+import { StyleSheet, Dimensions, Platform, View  } from 'react-native'
+import SliderEntry from '../utils/SliderEntry'
+
 export default class MentorScreen extends Component {
+  _renderItem ({item, index}) {
+    return (
+        <View style={styles.slide}>
+            <Text style={styles.title}>{ item.title }</Text>
+        </View>
+    );
+  }
+
+  _renderItemWithParallax ({item, index}, parallaxProps) {
+    return (
+        <SliderEntry
+          data={item}
+          even={(index + 1) % 2 === 0}
+          parallax={true}
+          parallaxProps={parallaxProps}
+        />
+    );
+}
+
   render() {
+
     return (
       <Container>
         <Header />
         <Content>
-
+          {console.log(this._renderItem)}
+          <Carousel
+              ref={(c) => { this._carousel = c; }}
+              data={ENTRIES1}
+              renderItem={this._renderItemWithParallax}
+              sliderWidth={sliderWidth}
+              itemWidth={itemWidth}
+          />
         </Content>
       </Container>
     );
   }
 }
+
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+
+function wp (percentage) {
+  const value = (percentage * viewportWidth) / 100;
+  return Math.round(value);
+}
+
+const slideHeight = viewportHeight * 0.36;
+const slideWidth = wp(75);
+const itemHorizontalMargin = wp(2);
+
+export const sliderWidth = viewportWidth;
+export const itemWidth = slideWidth + itemHorizontalMargin * 2;
+
+const ENTRIES1 = [
+  {
+      title: 'Carla Zara',
+      subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+      illustration: 'https://i.imgur.com/UYiroysl.jpg'
+  },
+  {
+      title: 'Earlier this morning, NYC',
+      subtitle: 'Lorem ipsum dolor sit amet',
+      illustration: 'https://i.imgur.com/UPrs1EWl.jpg'
+  },
+  {
+      title: 'White Pocket Sunset',
+      subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+      illustration: 'https://i.imgur.com/MABUbpDl.jpg'
+  },
+  {
+      title: 'Acrocorinth, Greece',
+      subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+      illustration: 'https://i.imgur.com/KZsmUi2l.jpg'
+  },
+  {
+      title: 'The lone tree, majestic landscape of New Zealand',
+      subtitle: 'Lorem ipsum dolor sit amet',
+      illustration: 'https://i.imgur.com/2nCt3Sbl.jpg'
+  },
+  {
+      title: 'Middle Earth, Germany',
+      subtitle: 'Lorem ipsum dolor sit amet',
+      illustration: 'https://i.imgur.com/lceHsT6l.jpg'
+  }
+];
 
 const styles = StyleSheet.create({
   button1: {
